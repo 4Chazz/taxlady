eff4e6ce266962e11d0648cf7c69fcf4e0785984
4chazz <chazzbrooks@gmail.com>
2018-03-26 22:02:57 +0000

    Add Scroll to Top button

--- app/assets/javascripts/active.js
+++ app/assets/javascripts/active.js
@@ -32,6 +32,17 @@
         Scroll Top
     -------------------------------------*/
 
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 50 ) {
+          $('.scrolltop:hidden').stop(true, true).fadeIn();
+      } else {
+          $('.scrolltop').stop(true, true).fadeOut();
+      }
+    });
+    $(function()
+      {$(".scroll").click(function()
+        {$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
+
     // $.scrollUp({
     //   scrollName: 'scrollUp', // Element ID
     //   topDistance: '300', // Distance from top before showing element (px)
